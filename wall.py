7621d44e53aa1a3e98834451b07c9b06d25b4734
julien <julien.calabro@gmail.com>
2021-05-05 12:28:27 +0200

    ajout matériau mur

--- wall.py
+++ wall.py
@@ -1,7 +1,12 @@
+
 class wall:
-    def __init__(self,xOrigin,yOrigin,xDirection, yDirection):
+    def __init__(self, xOrigin, yOrigin, xDirection, yDirection, material):
         self.n=[xDirection,yDirection]
         self.origin = [xOrigin,yOrigin]
+        self.material = material
+        self.relativePermitivity = 0
+        self.condctivity = 0
+
     def getOriginX(self):
         return self.origin[0]
     def getOriginY(self):
@@ -10,3 +15,16 @@ class wall:
         return self.n[0]
     def getNy(self):
         return self.n[1]
+
+    def set(self, material):
+        if material == "brick":
+            self.condctivity = 0.02
+            self.relativePermitivity = 4.6
+        elif material == "concrete":
+            self.condctivity = 0.014
+            self.relativePermitivity = 5
+        elif material == "partition":
+            self.condctivity = 0.04
+            self.relativePermitivity = 2.25
+        else:
+            print("matériau inconnu")
