1f747d1c56fb6156e5e9152a4cab697eb7014ee2
a-trillet <62527027+a-trillet@users.noreply.github.com>
2021-05-06 14:04:52 +0200

    création mapde base et du MET

---
+++ ImageMethod.py
@@ -0,0 +1,14 @@
+from Ray import Ray
+
+
+def getRayImages(originX, originY, walls):
+    rays = []
+    for wall in walls:
+        PsSN = (originX - wall.getOriginX())*wall.nX + (originY - wall.getOriginY())*wall.nY
+        if PsSN != 0:
+            ray = Ray(originX, originY)
+            ray.imagePoints.append()
+            rays.append(ray)
+
+
+    return rays
