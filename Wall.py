d854f4c9e9b7dca1b6f02358cc513034b0b9b161
julien <julien.calabro@gmail.com>
2021-05-07 02:34:33 +0200

    comentaire et parametre

--- Wall.py
+++ Wall.py
@@ -1,21 +1,26 @@
-from math import sqrt
-
-
+import math
+#calculer un maximum de choses à la création du mur pour ne as devoir les calculer après
+#si il y a un problème a l'exécution, il faut que le programme s'arrete immédiatement pour pas perdre de temps
+mu0 = 4 * math.pi * 10 ** (-7)
 class Wall:
     def __init__(self, xOrigin, yOrigin, xDirection, yDirection, material):
-        self.length = sqrt(xDirection^2+yDirection^2)
+        self.length = math.sqrt(xDirection ** 2 + yDirection ** 2)
         self.uX = xDirection/self.length                    #vecteur U // au mur
         self.uY = yDirection
         self.nX = yDirection                                #vecteur N normal au mur
         self.nY = -xDirection
 
-        self.origin = [xOrigin,yOrigin]
+        self.origin = [xOrigin, yOrigin]
 
         self.material = material
+        self.mat = -1
         self.relativePermitivity = 0
         self.condctivity = 0
+        self.thickness = 0.5                                #en mètre
         self.xDirection = xDirection                        #pour l'affichage
         self.yDirection = yDirection
+
+
     def getOriginX(self):
         return self.origin[0]
 
@@ -30,12 +35,15 @@ class Wall:
 
     def setProperty(self, material):
         if material == "brick":
+            self.mat = 0
             self.condctivity = 0.02
             self.relativePermitivity = 4.6
         elif material == "concrete":
+            self.mat = 1
             self.condctivity = 0.014
             self.relativePermitivity = 5
         elif material == "partition":
+            self.mat = 2
             self.condctivity = 0.04
             self.relativePermitivity = 2.25
         else:
