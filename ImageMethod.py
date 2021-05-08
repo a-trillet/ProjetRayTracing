322214e8195467463d51bc4ac91ccaabad957e14
a-trillet <62527027+a-trillet@users.noreply.github.com>
2021-05-08 19:06:18 +0200

    merge

--- ImageMethod.py
+++ ImageMethod.py
@@ -12,19 +12,19 @@ def getRayImages(originX, originY, walls, oldRay):
     if e < 2:
         for wall in walls:
             try:
-                if wall != oldRay.walls[-1]:
+                if  wall != oldRay.walls[-1]:
                     PsSN = (originX - wall.getOriginX()) * wall.nX + (originY - wall.getOriginY()) * wall.nY
                     if PsSN != 0:
                         ray = copy.deepcopy(oldRay)
                         ray.walls.append(wall)
                         ray.imagePoints.append([originX - 2 * PsSN * wall.nX, originY - 2 * PsSN * wall.nY])
-                        rays.extend(getRayImages(ray.imagePoints[e][0], ray.imagePoints[e][1], walls, ray))
-                    # else:
-                    # print("rayon // à un mur")
-                # else:
-                # print(wall.origin[0])
-            # else:
-            # print("chaud")
+                        rays.extend(getRayImages(ray.imagePoints[e][0], ray.imagePoints[e][1],walls, ray))
+                    #else:
+                        #print("rayon // à un mur")
+                #else:
+                    #print(wall.origin[0])
+        #else:
+            #print("chaud")
             except:
                 if oldRay.imagePoints == []:
                     PsSN = (originX - wall.getOriginX()) * wall.nX + (originY - wall.getOriginY()) * wall.nY
