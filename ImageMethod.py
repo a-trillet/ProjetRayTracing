e48d09ae283a81a0da1c1b373fbe5cc5f76e4189
a-trillet <62527027+a-trillet@users.noreply.github.com>
2021-05-08 13:43:45 +0200

    try dans image

--- ImageMethod.py
+++ ImageMethod.py
@@ -1,25 +1,43 @@
 import copy
-
+import Map
+#from main import walls
 from Ray import Ray
 
 
 # créer un ray de base Ray(originX, originY, receiverX, receiverY))
-def getRayImages(originX, originY, walls, oldRay):
+def getRayImages(originX, originY,walls, oldRay):
+
     rays = [oldRay]
     e = len(oldRay.imagePoints)
     if e < 3:
         for wall in walls:
-            if oldRay.walls == [] or wall != oldRay.walls[-1]:
-                PsSN = (originX - wall.getOriginX()) * wall.nX + (originY - wall.getOriginY()) * wall.nY
-                if PsSN != 0:
-                    ray = copy.deepcopy(oldRay)
-                    ray.walls.append(wall)
-                    ray.imagePoints.append([originX - 2 * PsSN * wall.nX, originY - 2 * PsSN * wall.nY])
-                    rays.extend(getRayImages(ray.imagePoints[e][0], ray.imagePoints[e][1], walls, ray))
+            try:
+                if  wall != oldRay.walls[-1]:
+                    PsSN = (originX - wall.getOriginX()) * wall.nX + (originY - wall.getOriginY()) * wall.nY
+                    if PsSN != 0:
+                        ray = copy.deepcopy(oldRay)
+                        ray.walls.append(wall)
+                        ray.imagePoints.append([originX - 2 * PsSN * wall.nX, originY - 2 * PsSN * wall.nY])
+                        rays.extend(getRayImages(ray.imagePoints[e][0], ray.imagePoints[e][1],walls, ray))
+                    #else:
+                        #print("rayon // à un mur")
                 #else:
-                    #print("rayon // à un mur")
-            #else:
-                #print(wall.origin[0])
-    #else:
-        #print("chaud")
+                    #print(wall.origin[0])
+        #else:
+            #print("chaud")
+            except:
+                if oldRay.imagePoints == []:
+                    PsSN = (originX - wall.getOriginX()) * wall.nX + (originY - wall.getOriginY()) * wall.nY
+                    if PsSN != 0:
+                        ray = copy.deepcopy(oldRay)
+                        ray.walls.append(wall)
+                        ray.imagePoints.append([originX - 2 * PsSN * wall.nX, originY - 2 * PsSN * wall.nY])
+                        rays.extend(getRayImages(ray.imagePoints[e][0], ray.imagePoints[e][1], walls, ray))
+                    # else:
+                    # print("rayon // à un mur")
+                # else:
+                # print(wall.origin[0])
+                # else:
+                # print("chaud")
+
     return rays
--- main.py
+++ main.py
@@ -5,10 +5,10 @@ import Display as dp
 import multiprocessing as mp
 
 MAPstyle = 1  # 1(corner) or 2(MET)
-walls = Map.getWalls(2)
-ray = Ray(100, 65, 100, 40)
-#rays = getRayImages(0, 0, walls, ray)
-#print(len(rays))
+walls = Map.getWalls(MAPstyle)
+ray = Ray(0, 0, 0, 5)
+rays = getRayImages(0, 0, walls, ray)
+print(len(rays))
 dp.display()
 
-print("Number of processors: ", mp.cpu_count())
+#print("Number of processors: ", mp.cpu_count())
